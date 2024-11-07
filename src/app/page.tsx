11881712601d5b7ee40f23f618ad97d4a5e3d583
Dean Lynch <lynchdean@gmail.com>
2024-11-07 17:57:58 +0000

    experience section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 import {SkillGrid} from "@/app/components/skill-grid";
 import {ProjectCard} from "@/app/components/project-card";
 import {SectionHeading} from "@/app/components/section-heading";
+import {TimelineEntry} from "@/app/components/timeline-entry";
+
+import { experience } from "@/../data/experience.js";
 
 export default function Home() {
   return (
@@ -18,22 +21,48 @@ export default function Home() {
               </div>
           </div>
 
+
           <div className="container mx-auto pb-8">
-              <SectionHeading heading="Skills:" />
-                <SkillGrid title="Languages" skills={["Python", "Java", "JavaScript", "TypeScript"]} />
-                <SkillGrid title="Frameworks" skills={["Django", "Flask", "React", "Next.js", "Tailwind CSS", "Bootstrap"]} />
-                <SkillGrid title="Tools" skills={["Git", "Selenium", "Jenkins", "Jira", "Docker", "AWS", "SQL", "Linux", "Windows"]} />
+              <SectionHeading heading="Experience:"/>
+              <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
+                  {experience.map((entry, index) => (
+                      <TimelineEntry
+                          key={index}
+                          year={entry.year}
+                          company={entry.company}
+                          title={entry.title}
+                          location={entry.location}
+                          description={entry.description}
+                      />
+                  ))}
+              </ul>
           </div>
 
           <div className="container mx-auto pb-8">
-              <SectionHeading heading="Projects:" />
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                    <ProjectCard title="pizzapool" github="lynchdean/pizzapool" desc={["A carpool-style group ordering web app.", "Used by one of the most popular run clubs in Dublin."]} tech={["Python", "Django"]}/>
-                    <ProjectCard title="Mild Watch Face" github="lynchdean/MildWatchFace " desc={["Garmin watch face for Mild Activity Run CLub"]} tech={["MonkeyC"]} />
-                    <ProjectCard title="Dean's Simple Watch Face" github="lynchdean/DeansSimpleWatchFace" desc={["A simple data-rich watch face for Garmin devices."]} tech={["MonkeyC"]} />
-                    <ProjectCard title="Portfolio" github="lynchdean/portfolio" desc={["Personal portfolio website (you are here)"]} tech={["React", "Next.js", "Tailwind CSS"]} />
-                    <ProjectCard title="BirdWatch Fingal" github="lynchdean/birdwatch-fingal" desc={["Website for BirdWatchIreland Fingal branch."]} tech={["React", "Bootstrap"]} />
-                </div>
+              <SectionHeading heading="Skills:"/>
+              <SkillGrid title="Languages" skills={["Python", "Java", "JavaScript", "TypeScript"]}/>
+              <SkillGrid title="Frameworks"
+                         skills={["Django", "Flask", "React", "Next.js", "Tailwind CSS", "Bootstrap"]}/>
+              <SkillGrid title="Tools"
+                         skills={["Git", "Selenium", "Jenkins", "Jira", "Docker", "AWS", "SQL", "Linux", "Windows"]}/>
+          </div>
+
+          <div className="container mx-auto pb-8">
+              <SectionHeading heading="Projects:"/>
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                  <ProjectCard title="pizzapool" github="lynchdean/pizzapool"
+                               desc={["A carpool-style group ordering web app.", "Used by one of the most popular run clubs in Dublin."]}
+                               tech={["Python", "Django"]}/>
+                  <ProjectCard title="Mild Watch Face" github="lynchdean/MildWatchFace "
+                               desc={["Garmin watch face for Mild Activity Run CLub."]} tech={["MonkeyC"]}/>
+                  <ProjectCard title="Dean's Simple Watch Face" github="lynchdean/DeansSimpleWatchFace"
+                               desc={["A simple data-rich watch face for Garmin devices."]} tech={["MonkeyC"]}/>
+                  <ProjectCard title="Portfolio" github="lynchdean/portfolio"
+                               desc={["Personal portfolio website. (you are here)"]}
+                               tech={["React", "Next.js", "Tailwind CSS"]}/>
+                  <ProjectCard title="BirdWatch Fingal" github="lynchdean/birdwatch-fingal"
+                               desc={["Website for BirdWatchIreland Fingal branch."]} tech={["React", "Bootstrap"]}/>
+              </div>
           </div>
       </div>
   );
