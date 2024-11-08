466c8342c4179a690f9b791416c780a72fce830c
Dean Lynch <lynchdean@gmail.com>
2024-11-08 10:53:14 +0000

    style

---
+++ data/skills.js
@@ -0,0 +1,14 @@
+export const skills = [
+    {
+        title: "Languages",
+        skills: ["Python", "Java", "JavaScript", "TypeScript"]
+    },
+    {
+        title: "Frameworks",
+        skills: ["Django", "Flask", "React", "Next.js", "Tailwind CSS", "Bootstrap"]
+    },
+    {
+        title: "Tools/Other",
+        skills: ["Git", "Selenium", "Jenkins", "Jira", "Docker", "AWS", "SQL", "Linux", "Windows"]
+    }
+]
