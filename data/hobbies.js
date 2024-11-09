cb83a54fc0ee4c30f45a2a44137f04659bb999b1
Dean Lynch <lynchdean@gmail.com>
2024-11-09 16:35:54 +0000

    image path fix + centre footer

--- data/hobbies.js
+++ data/hobbies.js
@@ -2,16 +2,16 @@ export const hobbies = [
     {
         title: "Running",
         description: "Running is currently a big part of my life. I'm a regular at Mild Activity in Dublin and I do a lot of extra training each week. I've ran two marathons so far and looking forward to signing up to one or two more in the next year.",
-        image: "portfolio/AF3E25AA-1E81-42F4-832E-24100B4240C2_1_201_a.jpeg"
+        image: "AF3E25AA-1E81-42F4-832E-24100B4240C2_1_201_a.jpeg"
     },
     {
         title: "Cycling",
         description: "I've started getting into road cycling over the past year, and I really enjoy it. When I'm outside of a marathon training block I try get out for long cycles some weekends - by myself or with a group.",
-        image: "portfolio/F1E92E78-AF5F-4539-B9BB-9A1534EA519C_1_201_a.jpeg"
+        image: "F1E92E78-AF5F-4539-B9BB-9A1534EA519C_1_201_a.jpeg"
     },
     {
         title: "Coffee",
         description: "I'm a big coffee person. I love trying out new brewing methods and different types of coffee. I have modded my own espresso machine (Gaggiuino project) which adds controllers and sensors to give extra control over shots, and a touchscreen to control it.",
-        image: "portfolio/8BB331EA-FA41-4DB3-98AA-A23F10DC5F9C_1_201_a.jpeg"
+        image: "8BB331EA-FA41-4DB3-98AA-A23F10DC5F9C_1_201_a.jpeg"
     }
 ];
