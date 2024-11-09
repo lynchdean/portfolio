33a4d6dd04bc60225c991cb94a1975f97d5c402b
Dean Lynch <lynchdean@gmail.com>
2024-11-09 14:36:34 +0000

    hobbies and bonus cats

---
+++ data/links.js
@@ -0,0 +1,18 @@
+export const links = [
+    {
+        title: "LinkedIn",
+        url: "https://www.linkedin.com/in/deanlynch97/"
+    },
+    {
+        title: "Github",
+        url: "https://www.github.com/lynchdean"
+    },
+    {
+        title: "pizzapool",
+        url: "https://pizzapool.app"
+    },
+    {
+        title: "Garmin CIQ Apps",
+        url: "https://apps.garmin.com/developer/ebe40baf-9d4c-4fde-b40c-b75cf3be799e/apps"
+    }
+];
