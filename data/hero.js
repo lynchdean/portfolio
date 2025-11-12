0dbd2351783187b9a1e34db49e47a1065438aaaa
Dean Lynch <lynchdean@gmail.com>
2025-11-12 15:50:03 +0000

    Update hero

--- data/hero.js
+++ data/hero.js
@@ -2,6 +2,6 @@ export const hero = {
     name: "Dean Lynch",
     title: "Software Engineer",
     degree: "BSc. Computer Applications & Software Engineering",
-    description1: "I am a software engineer based in Dublin, Ireland with a passion for full stack development. I have experience with a variety of technologies including React, Node.js, and Python.",
+    description1: "I am a software engineer based in Dublin, Ireland with a passion for software development. I have experience with a variety of technologies including Python, React, Node.js, and Java.",
     description2: "I am currently looking for new opportunities. If you would like to get in touch, please feel free to reach out to me on LinkedIn."
 };
